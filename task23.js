01e62c03b0fab882bbf99deae00bbec44b8f2220
frankyeyq <frankyeyq19892@163.com>
2016-06-14 22:42:18 +0800

    frank

--- task23.js
+++ task23.js
@@ -4,6 +4,7 @@ var postOrderBtn = document.getElementById('postOrder');
 var deepfirstBtn = document.getElementById('deepfirst');
 var breadthfirstBtn = document.getElementById('breadthfirst');
 var box1 = document.getElementsByClassName('box1')[0];
+var searchInput = document.getElementById('searchInput');
 var orderArr = [];
 
 function preOrder(node){
@@ -45,17 +46,6 @@ function deepOrder(node){
     }
 }
 
-
-function deepfirstBtn(node){
-    orderArr.push(node);
-    if(node.children.length){
-        for(var i = 0;i<node.children.length;i++){
-            deepOrder(node.children[i]);
-        }
-    }
-}
-
-
 function breadthOrder(node){
     var currentNode=[];
     currentNode.push(node); 
@@ -68,42 +58,100 @@ function breadthOrder(node){
     }
 
 }
+
+function deepfirstBtn(node){
+    orderArr.push(node);
+    if(node.children.length){
+        for(var i = 0;i<node.children.length;i++){
+            deepOrder(node.children[i]);
+        }
+    }
+}
+
+
+var orderFlag = true;
+
 function render(arr){
     for(var i = 0;i<document.getElementsByClassName('box').length;i++){
         document.getElementsByClassName('box')[i].style.backgroundColor="#fff";
     }
     var timer;
     var index = 0;
+    var searchFlag = true;
     timer = setInterval(function(){
+        orderFlag = false;
         if(index == orderArr.length){
             clearInterval(timer);
+            orderFlag = true;
+            orderArr =[];
+
         } else{
-            console.log(orderArr[index]);
-            if(index>0){
+            console.log(orderArr[index].firstChild.nodeValue.replace(/(^\s*)|(\s*$)/g, ""));
+            if(index>0&&searchFlag){
                 orderArr[index-1].style.backgroundColor = "#ffffff";
             }
             orderArr[index].style.backgroundColor = "#F125C2";
+            if(orderArr[index].firstChild.nodeValue.replace(/(^\s*)|(\s*$)/g, "")== searchInput.value){
+                searchFlag = false;
+                orderArr[index].style.backgroundColor = "#f00";
+            }else{
+                searchFlag = true;
+            }
             index++;
         }
     },600);
 }
 preOrderBtn.addEventListener('click',function(){
-    preOrder(box1);
-    render(orderArr);
+    if(orderFlag){
+        preOrder(box1);
+        render(orderArr);
+    }else{
+        alert("正在遍历！");
+    }
 });
 inOrderBtn.addEventListener('click',function(){
-    inOrder(box1);
-    render(orderArr);
+    if(orderFlag){
+        inOrder(box1);
+        render(orderArr);
+    }else{
+        alert("正在遍历！");
+    }
 });
 postOrderBtn.addEventListener('click',function(){
-    postOrder(box1);
-    render(orderArr);
+    if(orderFlag){
+        postOrder(box1);
+        render(orderArr);
+    }else{
+        alert("正在遍历！");
+    }
 });
 deepfirstBtn.addEventListener('click',function(){
-    deepOrder(box1);
-    render(orderArr);
+    if(orderFlag){
+        deepOrder(box1);
+        render(orderArr);
+    }else{
+        alert("正在遍历！");
+    }
 });
 breadthfirstBtn.addEventListener('click',function(){
-    breadthOrder(box1);
-    render(orderArr);
+    if(orderFlag){
+        breadthOrder(box1);
+        render(orderArr);
+    }else{
+        alert("正在遍历！");
+    }
 });
+window.onload = function(){
+    //为每个节点添加单击选中样式的事件
+    for(var i = 0;i<document.getElementsByClassName('box').length;i++){
+        document.getElementsByClassName('box')[i].onclick = function(e){
+            e.stopPropagation();
+            console.log(this.style.backgroundColor);
+            if(this.style.backgroundColor == "" ||this.style.backgroundColor == "rgb(255, 255, 255)") {
+              this.style.backgroundColor = "#987";
+            }else{
+                this.style.backgroundColor = "";
+            }
+        }
+    }
+}
